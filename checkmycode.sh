09fdc9daa2b47ecb011251539db8d0ad22fe9029
Sudeep <sudeep.rp@gmail.com>
2026-02-14 16:56:59 +0530

    exempt instagram from link check

--- checkmycode.sh
+++ checkmycode.sh
@@ -2,7 +2,7 @@
 set -e
 
 npx jscpd --min-lines 3 --min-tokens 20 --threshold 0 --gitignore --ignore "**/*.yml"
-markdown-link-check *.md
+markdown-link-check --config md-link-config.json *.md
 markdownlint --ignore vale --fix .
 # this will only check md files in the current directory.
 # because vale cannot ignore folders yet... and the vale README isn't compliant :)
